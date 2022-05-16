5adf782889624ca77b3536e9c46a56c39911e03c
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2022-05-16 11:22:06 -0300

    Adicionando os arquivos da aula 8

---
+++ Aula 8/js/owl/setup.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    pagination: false,
+});
